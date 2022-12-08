9148cf8fbada95346fc1a9621c705a98808a3109
gallarian <junckerc@isl-edu.be>
2022-12-08 02:06:44 +0100

    Add keyword final for homepage and dashboard controller

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+final class AdminController extends AbstractController
 {
     #[Route('/administration', name: 'admin_index', methods: ['GET'])]
     public function __invoke(): Response
--- src/Controller/Front/IndexController.php
+++ src/Controller/Front/IndexController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-class IndexController
+final class IndexController
 {
     #[Route('/', name: 'front_index', methods: ['GET'])]
     public function __invoke(Environment $twig): Response
