c536dfe4c12470cdb708179137aa464f56fcd44d
gallarian <junckerc@isl-edu.be>
2022-12-08 01:38:19 +0100

    Add homepage with tests

---
+++ src/Controller/Front/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @Author: Gallarian <gallarian@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class IndexController
+{
+    #[Route('/', name: 'front_index', methods: ['GET'])]
+    public function __invoke(Environment $twig): Response
+    {
+        return new Response($twig->render('front/views/index.html.twig'));
+    }
+}
