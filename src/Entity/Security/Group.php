cdf80168524cdb16da0afd66d6ff13d63503009e
gallarian <junckerc@isl-edu.be>
2022-12-08 02:22:31 +0100

    Feature: group (add entity)

---
+++ src/Entity/Security/Group.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * @Author: Gallarian <gallarian@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace App\Entity\Security;
+
+use App\Repository\Security\GroupRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\UuidV4;
+
+#[ORM\Entity(repositoryClass: GroupRepository::class), ORM\Table(name: 'ti_group')]
+class Group
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'uuid', unique: true)]
+    private UuidV4 $uuid;
+
+    #[ORM\Column(type: 'string', length: 100, unique: true)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $scope = null;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => 0])]
+    private bool $isActivate = false;
+
+    #[ORM\Column]
+    private DateTimeImmutable $createdAt;
+
+    /**
+     * @var DateTimeImmutable[]
+     */
+    #[ORM\Column(type: 'json')]
+    private array $updatedAt = [];
+
+    public function __construct()
+    {
+        $this->uuid = new UuidV4();
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): UuidV4
+    {
+        return $this->uuid;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getScope(): ?int
+    {
+        return $this->scope;
+    }
+
+    public function setScope(?int $scope): self
+    {
+        $this->scope = $scope;
+
+        return $this;
+    }
+
+    public function isIsActivate(): bool
+    {
+        return $this->isActivate;
+    }
+
+    public function setIsActivate(bool $isActivate): self
+    {
+        $this->isActivate = $isActivate;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTimeImmutable[]
+     */
+    public function getUpdatedAt(): array
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTimeImmutable[] $updatedAt
+     */
+    public function setUpdatedAt(array $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
